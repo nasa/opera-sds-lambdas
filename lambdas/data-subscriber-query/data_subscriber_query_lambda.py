41ae66c86096e2a121034fea27a5c12275c66e52
chrisjrd <chris.jrd@gmail.com>
2022-05-16 17:58:07 -0700

    feat(data-subscriber): re-adding bucket name param
    Refs #85

--- lambdas/data-subscriber-query/data_subscriber_query_lambda.py
+++ lambdas/data-subscriber-query/data_subscriber_query_lambda.py
@@ -81,8 +81,10 @@ def lambda_handler(event, context):
     job_type = os.environ['JOB_TYPE']
     job_release = os.environ['JOB_RELEASE']
     queue = os.environ['JOB_QUEUE']
+    isl_bucket_name = os.environ['ISL_BUCKET_NAME']
     job_spec = "job-%s:%s" % (job_type, job_release)
     job_params = {
+        "isl_bucket_name": isl_bucket_name,
         "minutes": minutes,
         "download_job_release": os.environ["JOB_RELEASE"],
         "download_job_queue": os.environ["DOWNLOAD_JOB_QUEUE"],
