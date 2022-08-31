2830a86ad5fb76693da59440ae4c7f5e57bad273
chrisjrd <chris.jrd@gmail.com>
2022-08-31 09:35:24 -0700

    feat(data-subscriber): move test dependencies from setup.py to requirements.txt
    Refs #199

--- setup.py
+++ setup.py
@@ -3,11 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='opera_sds_lambdas',
     version='0.0.1',
-    packages=find_packages(),
-    install_requires=[
-        "pytest>=7.1.1",
-        "pytest-mock>=3.8.2",
-        "pytest-asyncio>=0.18.3",
-        "pytest-cov>=3.0.0"
-    ]
+    packages=find_packages()
 )
