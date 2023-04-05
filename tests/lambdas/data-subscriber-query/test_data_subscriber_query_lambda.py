8f61fd047ae3421dcafa7d96bfe0405f205c270c
chrisjrd <chris.jrd@gmail.com>
2023-04-05 09:44:39 -0700

    feat(query-trigger): rolling window margin for temporal start date. fix bug with temporal start date param
    Refs #28

--- tests/lambdas/data-subscriber-query/test_data_subscriber_query_lambda.py
+++ tests/lambdas/data-subscriber-query/test_data_subscriber_query_lambda.py
@@ -1,8 +1,11 @@
+import datetime
 import importlib
 import os
 from unittest.mock import MagicMock
 
+import pytest
 from pytest_mock import MockerFixture
+from _pytest.monkeypatch import MonkeyPatch
 
 os.environ = {
     "MOZART_URL": "dummy_mozart_url",
@@ -23,7 +26,7 @@ os.environ = {
 data_subscriber_query = importlib.import_module("lambdas.data-subscriber-query.data_subscriber_query_lambda")
 
 
-def test_lambda_handler(mocker: MockerFixture, ):
+def test_lambda_handler(mocker: MockerFixture, monkeypatch: MonkeyPatch):
     # ARRANGE
     event = {
         "id": "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
@@ -39,9 +42,79 @@ def test_lambda_handler(mocker: MockerFixture, ):
     }
     context = MagicMock()
     mocker.patch(data_subscriber_query.__name__ + ".submit_job", return_value=200)
+    monkeypatch.setenv("USE_TEMPORAL", "false")
 
     # ACT
     response = data_subscriber_query.lambda_handler(event, context)
 
     # ASSERT
     assert response == 200
+
+
+def test_get_temporal_start_datetime__when_use_temporal_is_empty_string__then_throws_exception(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "")
+
+    # ASSERT
+    with pytest.raises(Exception):
+        # ACT
+        temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+
+def test_get_temporal_start_datetime__when_use_temporal_is_false__then_returns_none(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "false")
+
+    # ACT
+    temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+    # ASSERT
+    assert temporal_start_datetime == None
+
+
+def test_get_temporal_start_datetime__when_use_temporal_is_true__but_no_temporal_value_given__then_throws_exception(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "true")
+
+    # ASSERT
+    with pytest.raises(Exception):
+        # ACT
+        temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+
+def test_get_temporal_start_datetime__when_margin_given__then_returns_updated_datetime(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "true")
+    monkeypatch.setenv("TEMPORAL_START_DATETIME_MARGIN_DAYS", 3)
+
+    # ACT
+    temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+    # ASSERT
+    assert temporal_start_datetime == "2022-12-29T00:00:00Z"
+
+
+def test_get_temporal_start_datetime__when_datetime_given__then_returns_updated_datetime(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "true")
+    monkeypatch.setenv("TEMPORAL_START_DATETIME", "2022-12-29T00:00:00Z")
+
+    # ACT
+    temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+    # ASSERT
+    assert temporal_start_datetime == "2022-12-29T00:00:00Z"
+
+
+def test_get_temporal_start_datetime__when_both_margin_and_datetime_are_given__then_uses_margin(monkeypatch):
+    # ARRANGE
+    monkeypatch.setenv("USE_TEMPORAL", "true")
+    monkeypatch.setenv("TEMPORAL_START_DATETIME_MARGIN_DAYS", 3)
+    monkeypatch.setenv("TEMPORAL_START_DATETIME", "1970-01-01T00:00:00Z")
+
+    # ACT
+    temporal_start_datetime = data_subscriber_query.get_temporal_start_datetime(datetime.datetime.strptime("2023-01-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"))
+
+    # ASSERT
+    assert temporal_start_datetime == "2022-12-29T00:00:00Z"
+
