6c6d56424b4903366714e82abaa7486cc4141d30
FIXME <FIXME>
2025-08-25 17:26:09 -0700

    update

--- ci/jenkins/build-test-deploy/build.sh
+++ ci/jenkins/build-test-deploy/build.sh
@@ -18,10 +18,8 @@ echo "GIT_OAUTH_TOKEN: $GIT_OAUTH_TOKEN"
 TAG=$BRANCH
 echo "TAG: $TAG"
 
-# set env vars for python3.12 and activate
-export PATH=/home/hysdsops/miniforge3/bin:$PATH
+# activate python3.12 venv
 source /home/hysdsops/verdi-py3.12/bin/activate
-which python
 
 rm -rf ${WORKSPACE}/lambda_packages
 
