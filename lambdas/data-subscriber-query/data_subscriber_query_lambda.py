8d3259c5125f6f43360a5362ea7863a86e2679ce
hyunlee <Hyun.H.Lee@jpl.nasa.gov>
2025-11-19 08:55:20 -0800

    Fix typo of k_offsets_counts

--- lambdas/data-subscriber-query/data_subscriber_query_lambda.py
+++ lambdas/data-subscriber-query/data_subscriber_query_lambda.py
@@ -138,7 +138,7 @@ def _create_job(event: Dict):
         "max_revision": f'--max-revision={os.environ["MAX_REVISION"]}',
         "k": f"--k={cslc_processing_k}" if cslc_processing_k else "",
         "m": f"--m={cslc_processing_m}" if cslc_processing_m else "",
-        "k_offsets_counts": f"--k-offsets-counts="{k_offsets_counts}" if k_offsets_counts else "",
+        "k_offsets_counts": f"--k-offsets-counts={k_offsets_counts}" if k_offsets_counts else "",
         "grace_mins": f"--grace-mins={grace_mins}" if grace_mins else "",
         "coverage_percentage": f"--coverage-percentage={coverage_percentage}" if coverage_percentage else "",
         "coverage_num": f"--coverage-num={coverage_num}" if coverage_num else "",
