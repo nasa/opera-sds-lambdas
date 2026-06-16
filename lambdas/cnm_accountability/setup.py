973bd6d08b76e4d790fa1bfec2275803ba92a9f0
RKuttruff <72955101+RKuttruff@users.noreply.github.com>
2026-06-16 13:52:38 -0700

    add query module & template to package

--- lambdas/cnm_accountability/setup.py
+++ lambdas/cnm_accountability/setup.py
@@ -177,7 +177,9 @@ class Package(setuptools.Command):
         self.execute(
             "zip -9 {package_name} {files}".format(
                 package_name=lambda_package,
-                files=' '.join(glob.glob("lambda_function.py"))))
+                files=' '.join(
+                    glob.glob("lambda_function.py") + glob.glob("queries.py") + glob.glob("*.jinja2")
+                )))
         os.chdir(os.path.join(self.workspace, WHEELHOUSE))
         self.execute(
             "zip -rg ../{dist}/{archive_name}-{version}.zip "
